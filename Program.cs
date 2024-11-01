535e2b95d3d5d80267e16f6c300f05900054aaeb
John Roberts <bigjlr2004@yahoo.com>
2024-11-01 17:52:37 -0500

    First Push

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ThrownForALoop
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string greeting = @"Welcome to thrown for a loop.
+            Your one stop shop for used sporting equipment.";
+            Console.WriteLine(greeting);
+            Console.WriteLine("Please choose an option: ");
+            string theMeaningOfLife = "forty-two";
+            Console.WriteLine(theMeaningOfLife);
+            Console.Write("Please enter a product line: ");
+            string response = Console.ReadLine().Trim();
+            while (string.IsNullOrEmpty(response))
+            {
+                Console.WriteLine("You didn't type anything.");
+                Console.Write("Try again: ");
+                response = Console.ReadLine().Trim();
+            }
+
+            Console.WriteLine(@$"You chose: {response}. 
+Thank you for your inputs.");
+
+
+
+        }
+    }
+}
